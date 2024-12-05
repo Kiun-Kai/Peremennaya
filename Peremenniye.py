3c6ebad0e067c53ce7d10ae1985e54bfca727e2b
Kiun-Kai <kiunkaims@gmail.com>
2024-12-05 13:09:23 +0300

    Add files via upload

---
+++ Peremenniye.py
@@ -0,0 +1,5 @@
+number_of_completed_HW=12
+number_of_hours_spent=1.5
+name_of_course=" курс:Python,"
+time_for_one_task=number_of_hours_spent/number_of_completed_HW
+print(name_of_course,'всего задач:',number_of_completed_HW,',затрачено часов:', number_of_hours_spent,',среднее выполнение одной задачи:', time_for_one_task)
